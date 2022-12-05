eff773eeb73e8bc29564b3da81169d821290366b
DESKTOP-16C8CU5\bjoer <langb4@bfh.ch>
2022-12-05 11:32:19 +0100

    added detail html

--- api.py
+++ api.py
@@ -10,16 +10,21 @@ BASE = 'http://127.0.0.1:5000/'
 
 @api.route('/')
 def home():
-    return render_template('index.html', info = 'Welcome to the API page!')
+    response = requests.get(BASE + 'sight/all')
+    print(response.json())
+    return render_template('index.html', info = response.json())
 
 
 @api.route('/detail/<sight_id>')
 def get_json(sight_id):
     response = requests.get(BASE + 'sight/' + sight_id)
-    return render_template('index.html', 
+    return render_template('detail.html', 
                             id = sight_id,
                             info = response.json()['text'],
                             name = response.json()['name'],
                             zip = response.json()['zip'],)
+
+
+
 
 
